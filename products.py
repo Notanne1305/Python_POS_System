31faa8d362f28d209763c665f342531591df6f45
Notanne1305 <j.sindo.545117@umindanao.edu.ph>
2025-12-16 19:01:25 +0800

    ana si Charisse

--- products.py
+++ products.py
@@ -2,6 +2,14 @@ products = {
     1: {"Product": "Burger", "price": 50},
     2: {"Product": "Fries", "price": 30},
     3: {"Product": "Spaghetti", "price": 50},
+    4: {"Product": "1-pc Chicken with Rice", "price": 89},
+    5: {"Product": "Hotdog Sandwich", "price": 35},
+    6: {"Product": "Regular Coke", "price": 25},
+    7: {"Product": "Iced Tea", "price": 30},
+    8: {"Product": "Milkshake", "price": 55},
+    9: {"Product": "Ice Cream Sundae", "price": 35},
+    10: {"Product": "Brownie", "price": 25},
+    
 }
 
 def show_products():
